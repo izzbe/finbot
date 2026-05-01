822314b7936742ffbc3222903e8eeacbd778c0ba
Ian Zhang <ianzhang08030612@gmail.com>
2026-05-01 19:20:02 -0400

    first commit

---
+++ backend/packages/common/src/common/logging.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setLevel(level)
+        formatter = logging.Formatter(
+            fmt="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    logger.setLevel(level)
+    return logger
+
